c488c88e2b3fc2ef1b175da7d014dfc5f0d18778
Pham Nguyen Bao Tran <248277356+pnbtran@users.noreply.github.com>
2026-08-01 23:04:12 +0300

    Add lesson 18: Class Person

---
+++ 018_OOP_Person/018_OOP_Person/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _018_OOP_Person
+{
+    internal class Program
+    {
+        // TAO CLASS PERSON
+        public class Person
+        {
+            public string Name {get; set;}  
+            public int Age {get; set;}  
+
+            public string Address {get; set;}
+        }
+
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();       // Tao object từ class Person
+            person1.Name = "Nguyen Van A";       // gan thuoc tinh Name
+            person1.Age = 25;
+            person1.Address = "TP Ho Chi Minh";
+
+            Console.WriteLine($"Ten: {person1.Name}");
+            Console.WriteLine($"Tuoi: {person1.Age}");
+            Console.WriteLine($"Dia chi: {person1.Address}");
+
+            Console.WriteLine(" ");
+
+            // Tao them doi tuong Person khac
+            Person person2 = new Person();
+            person2.Name = "Tran Thi B";
+            person2.Age = 30;
+            person2.Address = "Thu do Ha Noi";
+            Console.WriteLine($"{person2.Name} - {person2.Age} tuoi - {person2.Address}");
+        }
+    }
+}
---
+++ 018_OOP_Person/README.md
@@ -0,0 +1,23 @@
+# 018 _ TẠO CLASS NGƯỜI VỚI THUỘC TÍNH TÊN, TUỔI
+## Tóm tắt
+- Phân biệt **class** và **object**.
+- Khai báo class với thuộc tính (property) `{get; set;}`
+- Tuy cập thuộc tính qua dấu chấm.
+- Hiểu cơ bản về lập trình hướng đối tượng OOP.
+
+## Thuật toán
+- Khai báo class Person
+```csharp
+public class Person
+{
+    public string Name { get; set; }    // thuộc tính Name (chuỗi)
+    public int Age { get; set; }        // thuộc tính Age (số nguyên)
+}
+```
+Trong đó: 
+    - `public`: cho phép truy cập ở bất cứ đâu trong chương trình. 
+    - `string/ int`: kiểu dữ liệu của thuộc tính. 
+    - `{get; set;}: cú pháp "auto-property" - cho phép đọc (get) và gán (set) -> Khai báo thuộc tính chuẩn, gọn ràng. 
+
+## Lưu ý 
+- Class = bản thiết kế (khuôn mẫu); Object = sản phẩm thật tạo từ class bằng new. Class gói thuộc tính (dữ liệu) + phương thức (hành vi) vào một chỗ.
